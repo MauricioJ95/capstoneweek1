d85964689be261bebfe8e91c6c590c490a7a4c03
Mauricio Reyna <mauriciojreyna@gmail.com>
2019-01-18 17:00:39 -0500

    trying something different

---
+++ Capstoneweek1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstoneweek1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence to get translated into Pig Latin!");
+            string sentence = Console.ReadLine();
+
+        }
+
+        private static string TranslateToPigLatin(string sentence)
+        {
+            const string vowels = "AEIOUaeio";
+            List<string> pigWords = new List<string>();
+
+            foreach (string word in sentence.Split(' '))
+            {
+                string firstLetter = word.Substring(0, 1);
+                string restOfWord = word.Substring(1, word.Length - 1);
+                int currentLetter = vowels.IndexOf(firstLetter);
+
+                if (currentLetter == -1)
+                {
+                    pigWords.Add(restOfWord + firstLetter + "ay");
+                }
+                else
+                {
+                    pigWords.Add(word + "way");
+                }
+            }
+            return string.Join(" ", pigWords);
+        }
+    }
+}
